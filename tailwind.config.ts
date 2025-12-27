cc5236c906bf464bd3309416c3b22b6739d9ea78
sawmonhan <sawmon71293@gmail.com>
2025-12-27 16:11:50 +0630

    font applied

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontfamily: {
+    fontFamily: {
       sans:['Inter', 'sans-serif']
     },
     extend: {
