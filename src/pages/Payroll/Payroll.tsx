7c78eec0b8b3f94f664de536f4442652065b31a6
sawmonhan <sawmon71293@gmail.com>
2026-01-18 19:39:53 +0630

    blob error

--- src/pages/Payroll/Payroll.tsx
+++ src/pages/Payroll/Payroll.tsx
@@ -69,7 +69,7 @@ export default function PayrollList() {
       setError("")
       setLoading(true)
       const resp = await PayrollService.processPayroll({ payrollMonth: cleanMonthYear(monthYear) })
-      if (!resp.isSuccess){
+      if (!resp.isSuccess) {
         setError(resp.message)
         return
       }
@@ -120,235 +120,243 @@ export default function PayrollList() {
       const { blob, contentDisposition } = await exportReport(requestPayload);
       downloadFile(blob, contentDisposition);
       setExporting(false)
-    } catch (err) {
-      setError(err.message)
-      setExporting(false)
-    }
-  };
+    } catch (err: any) {
+        console.error(err);
 
-  const location = useLocation();
+        setError(
+          err?.message ??
+          err?.error ??
+          "Failed to export report"
+        );
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const payrollSummary = await PayrollService.fetchPayrollSummary(
-          {
-            MonthYear: "",
-            PageNo: currentPage,
-            PageSize: rowsPerPage
-          }
-        )
-        setData(payrollSummary?.data?.items as PayrollSummary[] ?? [])
-      } catch (err) {
-        console.log(err)
-        setError("Failed to fetch payroll summary")
+        setExporting(false);
       }
 
-    })()
-  }, [location.state])
+    };
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const payrollSummary = await PayrollService.fetchPayrollSummary(
-          {
-            MonthYear: cleanMonthYear(monthYear) ?? "",
-            PageNo: currentPage,
-            PageSize: rowsPerPage
-          }
-        )
-        setData(payrollSummary?.data?.items as PayrollSummary[] ?? [])
-      } catch (err) {
-        console.log(err)
-        setError("Failed to fetch payroll summary")
-      }
+    const location = useLocation();
 
-    })()
+    useEffect(() => {
+      (async () => {
+        try {
+          const payrollSummary = await PayrollService.fetchPayrollSummary(
+            {
+              MonthYear: "",
+              PageNo: currentPage,
+              PageSize: rowsPerPage
+            }
+          )
+          setData(payrollSummary?.data?.items as PayrollSummary[] ?? [])
+        } catch (err) {
+          console.log(err)
+          setError("Failed to fetch payroll summary")
+        }
 
-  }, [monthYear])
+      })()
+    }, [location.state])
 
-  return (
-    <div className="p-6 w-full">
-      <div className="flex flex-col md:flex-row gap-2 mb-4 w-full">
-        <p className="page-title">Payroll Summary</p>
-        {/* search */}
-        <div className="w-full md:w-auto text-primary-800">
-          <MonthYearPicker
-            value={monthYear}
-            onChange={(d) => setMonthYear(d)}
-            startYear={2000}
-            endYear={2035}
-          />
-          {error && (
-            <div className="mt-3 flex items-center gap-2 bg-red-50 border border-red-300 text-red-700 p-3 rounded-md text-sm">
-              <AlertCircle className="w-4 h-4" />
-              <span>{error}</span>
-            </div>
-          )}
-        </div>
-        {/* buttons */}
-        <Button className="primary-btn w-full md:w-auto" onClick={() => setOpen(true)}>
-          <FolderUp />
-          Export
-        </Button>
-        <Button disabled={loading} className="primary-btn w-full md:w-auto" onClick={processPayroll}><BadgeDollarSign />{loading ? 'processing...': 'Process Payroll'}</Button>
-      </div>
-      <Table className="w-full overflow-auto shadow-sm rounded-md">
-        <TableHeader className="bg-primary-300">
-          <TableRow className="border-none">
-            <TableHead >
-              No
-            </TableHead>
-            <TableHead >
-              Payroll Month
-            </TableHead>
-            <TableHead >
-              Total Working Days
-            </TableHead>
-            <TableHead >
-              Total Employees
-            </TableHead>
-            <TableHead >
-              Total Working Hours
-            </TableHead>
-            <TableHead >
-              Total Leave Hours
-            </TableHead>
-            <TableHead >
-              Total Actual Working Hours
-            </TableHead>
-            <TableHead >
-              Total Base Salary
-            </TableHead>
-            <TableHead >
-              Total Net Pay
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {currentData.length ?
-            currentData.map((payroll, index) => (
-              <TableRow
-                key={index}
-                className="odd:bg-primary-100 even:bg-primary-50 hover:bg-primary-200 transition-colors border-none py-3"
-                onClick={() => handleRowClick(payroll.payrollSummaryCode)}
-              >
-                <TableCell>{index + 1}</TableCell>
-                <TableCell>{payroll.payrollMonth}</TableCell>
-                <TableCell>{payroll.totalWorkingDays}</TableCell>
-                <TableCell>{payroll.employeeCount}</TableCell>
-                <TableCell>{payroll.totalWorkingHours}</TableCell>
-                <TableCell>{payroll.totalLeaveHours}</TableCell>
-                <TableCell>{payroll.totalActualWorkingHours}</TableCell>
-                <TableCell>{payroll.totalBaseSalary}</TableCell>
-                <TableCell>{payroll.totalNetPay}</TableCell>
-              </TableRow>
-            )) : (
-              <TableRow key="no-data">
-                <TableCell colSpan={8} className="h-24 text-center">
-                  <div className="flex items-center justify-center text-primary-500">
-                    No Data.
-                  </div>
-                </TableCell>
-              </TableRow>
+    useEffect(() => {
+      (async () => {
+        try {
+          const payrollSummary = await PayrollService.fetchPayrollSummary(
+            {
+              MonthYear: cleanMonthYear(monthYear) ?? "",
+              PageNo: currentPage,
+              PageSize: rowsPerPage
+            }
+          )
+          setData(payrollSummary?.data?.items as PayrollSummary[] ?? [])
+        } catch (err) {
+          console.log(err)
+          setError("Failed to fetch payroll summary")
+        }
+
+      })()
+
+    }, [monthYear])
+
+    return (
+      <div className="p-6 w-full">
+        <div className="flex flex-col md:flex-row gap-2 mb-4 w-full">
+          <p className="page-title">Payroll Summary</p>
+          {/* search */}
+          <div className="w-full md:w-auto text-primary-800">
+            <MonthYearPicker
+              value={monthYear}
+              onChange={(d) => setMonthYear(d)}
+              startYear={2000}
+              endYear={2035}
+            />
+            {error && (
+              <div className="mt-3 flex items-center gap-2 bg-red-50 border border-red-300 text-red-700 p-3 rounded-md text-sm">
+                <AlertCircle className="w-4 h-4" />
+                <span>{error}</span>
+              </div>
             )}
-        </TableBody>
-      </Table>
-
-      {/* Paginations */}
-      <div className="flex flex-col md:flex-row gap-2 items-center justify-between p-4 border-t">
-        {/* Left: Showing rows */}
-        <div className="text-sm text-muted-foreground">
-          {startRow}–{endRow} of {totalRows}
+          </div>
+          {/* buttons */}
+          <Button className="primary-btn w-full md:w-auto" onClick={() => setOpen(true)}>
+            <FolderUp />
+            Export
+          </Button>
+          <Button disabled={loading} className="primary-btn w-full md:w-auto" onClick={processPayroll}><BadgeDollarSign />{loading ? 'processing...' : 'Process Payroll'}</Button>
         </div>
+        <Table className="w-full overflow-auto shadow-sm rounded-md">
+          <TableHeader className="bg-primary-300">
+            <TableRow className="border-none">
+              <TableHead >
+                No
+              </TableHead>
+              <TableHead >
+                Payroll Month
+              </TableHead>
+              <TableHead >
+                Total Working Days
+              </TableHead>
+              <TableHead >
+                Total Employees
+              </TableHead>
+              <TableHead >
+                Total Working Hours
+              </TableHead>
+              <TableHead >
+                Total Leave Hours
+              </TableHead>
+              <TableHead >
+                Total Actual Working Hours
+              </TableHead>
+              <TableHead >
+                Total Base Salary
+              </TableHead>
+              <TableHead >
+                Total Net Pay
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {currentData.length ?
+              currentData.map((payroll, index) => (
+                <TableRow
+                  key={index}
+                  className="odd:bg-primary-100 even:bg-primary-50 hover:bg-primary-200 transition-colors border-none py-3"
+                  onClick={() => handleRowClick(payroll.payrollSummaryCode)}
+                >
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>{payroll.payrollMonth}</TableCell>
+                  <TableCell>{payroll.totalWorkingDays}</TableCell>
+                  <TableCell>{payroll.employeeCount}</TableCell>
+                  <TableCell>{payroll.totalWorkingHours}</TableCell>
+                  <TableCell>{payroll.totalLeaveHours}</TableCell>
+                  <TableCell>{payroll.totalActualWorkingHours}</TableCell>
+                  <TableCell>{payroll.totalBaseSalary}</TableCell>
+                  <TableCell>{payroll.totalNetPay}</TableCell>
+                </TableRow>
+              )) : (
+                <TableRow key="no-data">
+                  <TableCell colSpan={8} className="h-24 text-center">
+                    <div className="flex items-center justify-center text-primary-500">
+                      No Data.
+                    </div>
+                  </TableCell>
+                </TableRow>
+              )}
+          </TableBody>
+        </Table>
 
-        {/* Middle: Page buttons */}
+        {/* Paginations */}
+        <div className="flex flex-col md:flex-row gap-2 items-center justify-between p-4 border-t">
+          {/* Left: Showing rows */}
+          <div className="text-sm text-muted-foreground">
+            {startRow}–{endRow} of {totalRows}
+          </div>
 
-        <div className="flex space-x-1">
-          <button
-            onClick={goToFirst}
-            disabled={currentPage === 1}
-            className="px-2 py-1 rounded pagination-btn disabled:opacity-50"
-          >
-            <ChevronsLeft />
-          </button>
-          <button
-            onClick={goPrev}
-            disabled={currentPage === 1}
-            className="px-2 py-1 rounded pagination-btn disabled:opacity-50"
-          >
-            <ChevronLeft />
-          </button>
-          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+          {/* Middle: Page buttons */}
+
+          <div className="flex space-x-1">
             <button
-              key={page}
-              onClick={() => setCurrentPage(page)}
-              className={`px-3 py-1 rounded ${page === currentPage
-                ? "bg-primary-500 text-natural-50"
-                : "bg-natural-50 text-black hover:bg-gray-200"
-                }`}
+              onClick={goToFirst}
+              disabled={currentPage === 1}
+              className="px-2 py-1 rounded pagination-btn disabled:opacity-50"
             >
-              {page}
+              <ChevronsLeft />
             </button>
-          ))}
-          <button
-            onClick={goNext}
-            disabled={currentPage === totalPages}
-            className="px-2 py-1 rounded pagination-btn disabled:opacity-50"
-          >
-            <ChevronRight />
-          </button>
-          <button
-            onClick={goToLast}
-            disabled={currentPage === totalPages}
-            className="px-2 py-1 rounded pagination-btn disabled:opacity-50"
-          >
-            <ChevronsRight />
-          </button>
-        </div>
-
-        {/* Right: Rows per page */}
-        <div className="flex items-center space-x-2">
-          <span className="text-sm text-muted-foreground">Rows/page:</span>
-          <select
-            value={rowsPerPage}
-            onChange={(e) => {
-              setRowsPerPage(Number(e.target.value));
-              setCurrentPage(1); // reset page
-            }}
-            className="border rounded px-2 py-1 text-sm p-3"
-          >
-            {[10, 20, 30, 50].map((n) => (
-              <option key={n} value={n}>
-                {n}
-              </option>
+            <button
+              onClick={goPrev}
+              disabled={currentPage === 1}
+              className="px-2 py-1 rounded pagination-btn disabled:opacity-50"
+            >
+              <ChevronLeft />
+            </button>
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+              <button
+                key={page}
+                onClick={() => setCurrentPage(page)}
+                className={`px-3 py-1 rounded ${page === currentPage
+                  ? "bg-primary-500 text-natural-50"
+                  : "bg-natural-50 text-black hover:bg-gray-200"
+                  }`}
+              >
+                {page}
+              </button>
             ))}
-          </select>
+            <button
+              onClick={goNext}
+              disabled={currentPage === totalPages}
+              className="px-2 py-1 rounded pagination-btn disabled:opacity-50"
+            >
+              <ChevronRight />
+            </button>
+            <button
+              onClick={goToLast}
+              disabled={currentPage === totalPages}
+              className="px-2 py-1 rounded pagination-btn disabled:opacity-50"
+            >
+              <ChevronsRight />
+            </button>
+          </div>
+
+          {/* Right: Rows per page */}
+          <div className="flex items-center space-x-2">
+            <span className="text-sm text-muted-foreground">Rows/page:</span>
+            <select
+              value={rowsPerPage}
+              onChange={(e) => {
+                setRowsPerPage(Number(e.target.value));
+                setCurrentPage(1); // reset page
+              }}
+              className="border rounded px-2 py-1 text-sm p-3"
+            >
+              {[10, 20, 30, 50].map((n) => (
+                <option key={n} value={n}>
+                  {n}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
+        <ExportDateDialog
+          open={open}
+          onOpenChange={setOpen}
+          title="Export Payroll"
+          loading={exporting}
+          error={error}
+          onConfirm={({ range, format }) => {
+            handleExport({
+              from: range.from!,
+              to: range.to!,
+              format,
+              type: "admin",
+              name: "Payroll"
+            });
+          }}
+        />
+        <SuccessDialog
+          open={successDialogOpen}
+          onOpenChange={setSuccessDialogOpen}
+          onConfirm={handleSuccessConfirm}
+          title="Payroll Process"
+          description="Payroll processing completed"
+        />
       </div>
-      <ExportDateDialog
-        open={open}
-        onOpenChange={setOpen}
-        title="Export Payroll"
-        loading={exporting}
-        error={error}
-        onConfirm={({ range, format }) => {
-          handleExport({
-            from: range.from!,
-            to: range.to!,
-            format,
-            type: "admin",
-            name: "Payroll"
-          });
-        }}
-      />
-      <SuccessDialog
-        open={successDialogOpen}
-        onOpenChange={setSuccessDialogOpen}
-        onConfirm={handleSuccessConfirm}
-        title="Payroll Process"
-        description="Payroll processing completed"
-      />
-    </div>
-  );
-}
+    );
+  }
